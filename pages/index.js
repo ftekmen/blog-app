4f023a2a63f1b2f0a03ae542d6b1b7739d584c51
ftekmen <ftekmen@gmail.com>
2021-08-24 19:52:10 +0300

    pages and components are added, hero component created

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+import Hero from '../components/homepage/hero';
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+}
+
+export default HomePage;
