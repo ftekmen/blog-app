845fa1a6548d212a0f096418884393f47eddc54c
ftekmen <ftekmen@gmail.com>
2021-08-25 23:16:49 +0300

    contact form db connection created

---
+++ pages/api/contact.js
@@ -0,0 +1,39 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    // Store it in a database
+    const newMessage = { email, name, message };
+
+    let client;
+    try {
+      client = await MongoClient.connect('mongodb+srv://blog-user:YrjrUP68QMvpx7zp@cluster0.luplk.mongodb.net/blog-app?retryWrites=true&w=majority');
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database!' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!'});
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: 'Successfully stored message!', message: newMessage });
+  }
+}
+
+export default handler;
