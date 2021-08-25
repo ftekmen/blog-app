dfb1d8cd9f7d47879ed7efa53882b5360f7a205f
ftekmen <ftekmen@gmail.com>
2021-08-25 22:02:38 +0300

    post detail updated, contact form created

--- components/posts/post-detail/postContent.js
+++ components/posts/post-detail/postContent.js
@@ -1,23 +1,41 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown/react-markdown.min';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './PostHeader';
 import classes from './postContent.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started With NextJS',
-  image: 'getting-started-nextjs.png',
-  excerpt: 'NextJs is the React Framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-  date: '2022-02-10',
-  content: '# This is a first post'
-};
+function PostContent(props) {
+  const { post } = props;
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p: ({ node, ...props }) => {
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0].properties;
+
+        return (
+          <div className={classes.image}>
+            <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+          </div>
+        );
+      }
+
+      return <p>{props.children}</p>;
+    },
+    code: ({ node }) => {
+      // console.log(node);
+      // console.log(SyntaxHighlighter.supportedLanguages);
+      return <SyntaxHighlighter language="javascript" children={node.children[0].value} style={atomDark} />;
+    }
+  };
+
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,7 @@
-function ContactPage() {
+import ContactForm from "../components/contact/contactForm";
 
+function ContactPage() {
+  return <ContactForm />
 }
 
 export default ContactPage;
