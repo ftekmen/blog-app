2f22f674ebe2a9fa146473f7d27d5f766389d6c0
ftekmen <ftekmen@gmail.com>
2021-08-26 08:39:23 +0300

    meta data is added to pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
-import FeaturedPosts from '../components/homepage/featuredPosts';
+import Head from 'next/head';
 
+import FeaturedPosts from '../components/homepage/featuredPosts';
 import Hero from '../components/homepage/hero';
 import { getFeaturedPosts } from '../lib/postsUtil';
 
@@ -8,6 +9,10 @@ function HomePage(props) {
   console.log(props.posts);
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to My Blog</title>
+        <meta name="description" content="I post about programming and web development." />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,19 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+
 import AllPosts from "../../components/posts/allPosts";
 import { getAllPosts } from "../../lib/postsUtil";
 
 function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />
+  return (
+    <Fragment>
+      <Head>
+        <title>All Posts</title>
+        <meta name="description" content="A list of all programming-related tutoarials and posts!" />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps() {
