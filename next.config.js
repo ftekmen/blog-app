a95c286050c6ef8a947d640957945693a221a257
ftekmen <ftekmen@gmail.com>
2021-09-02 22:56:54 +0300

    config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-const withTM = require("next-transpile-modules")(["react-markdown"]);
-module.exports = withTM({
-  env: {
-    mongodb_username: 'blog-user',
-    mongodb_password: 'YrjrUP68QMvpx7zp',
-    mongodb_cluster: 'cluster0',
-    mongodb_database: 'blog-app'
-  },
-  webpack5: false
-});
+// const withTM = require("next-transpile-modules")(["react-markdown"]);
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'blog-user',
+        mongodb_password: 'TeqpgzNz2cgArLRm',
+        mongodb_cluster: 'cluster0',
+        mongodb_database: 'blog-app-dev'
+      }
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'blog-user',
+      mongodb_password: 'TeqpgzNz2cgArLRm',
+      mongodb_cluster: 'cluster0',
+      mongodb_database: 'blog-app'
+    }
+  };
+};
